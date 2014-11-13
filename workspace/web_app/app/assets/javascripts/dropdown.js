0be47c71e76ea25a940d7ab0c229321c7a86797c
Josh Nussbaum <nussbaum.joshua@gmail.com>
2014-11-13 10:40:27 +0000

    Add smooth scroll

--- workspace/web_app/app/assets/javascripts/dropdown.js
+++ workspace/web_app/app/assets/javascripts/dropdown.js
@@ -10,3 +10,18 @@ $(document).ready(function(){
         }
     });
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#]):not(.carousel-control)').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
