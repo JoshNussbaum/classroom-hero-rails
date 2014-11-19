db0dd4f6786c37ec1f1d5b9b13ac0b5372555bdb
Josh Nussbaum <nussbaum.joshua@gmail.com>
2014-11-19 22:40:28 +0000

    Embed video and fill in How it Works

--- workspace/web_app/app/assets/javascripts/dropdown.js
+++ workspace/web_app/app/assets/javascripts/dropdown.js
@@ -9,6 +9,17 @@ $(document).ready(function(){
             100);
         }
     });
+    
+      $("#closeDropDown").click(function(){
+        if($(".how-it-works-panel").css("display") == "none"){
+            setTimeout(function(){$(".how-it-works-panel").slideDown(600);}, 
+            100);
+        }
+        else{
+            setTimeout(function(){$(".how-it-works-panel").slideUp(600);}, 
+            100);
+        }
+    });
 });
 
 $(function() {
