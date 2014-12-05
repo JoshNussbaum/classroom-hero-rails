b56feeb2a5afac91758a4e92449fb188cc7055a5
Josh Nussbaum <nussbaum.joshua@gmail.com>
2014-12-05 06:39:51 -0800

    info blobs, about page, scrolldeck
    i’m tired.

--- workspace/web_app/app/assets/javascripts/application.js
+++ workspace/web_app/app/assets/javascripts/application.js
@@ -16,6 +16,11 @@
 //= require bootstrap-modalmanager
 //= require dropdown
 //= require jquery
+//= require jquery-1.8.2.min
+//= require jquery.easing.1.3
+//= require jquery.scrolldeck
+//= require jquery.scrollorama
+//= require jquery.scrollTo-1.4.3.1.min
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery-ui
--- workspace/web_app/app/assets/javascripts/dropdown.js
+++ workspace/web_app/app/assets/javascripts/dropdown.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var deck = new $.scrolldeck();
+
     $("#how-it-works-btn").click(function(){
         if($(".how-it-works-panel").css("display") == "none"){
             setTimeout(function(){$(".how-it-works-panel").slideDown(600);}, 
