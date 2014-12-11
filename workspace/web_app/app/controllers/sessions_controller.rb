7056fc48de3c421fe41f547c4025045f8936c291
goldcountry <bjamingord@gmail.com>
2014-12-11 12:53:59 -0800

    Fixed incorrect password handling

--- workspace/web_app/app/controllers/sessions_controller.rb
+++ workspace/web_app/app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   		log_in(user)
       redirect_to user
   	else
+      flash.now[:error] = "Invalid email/password combination"
   		render 'new'
   	end
   end
